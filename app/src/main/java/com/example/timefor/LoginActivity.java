2ccd94b8dcf1892f852ea4d056783efaf913f1d7
Teresiah6 <wangariteresiah6@gmail.com>
2020-04-30 21:23:30 +0300

    commit updated for sign up

--- app/src/main/java/com/example/timefor/LoginActivity.java
+++ app/src/main/java/com/example/timefor/LoginActivity.java
@@ -2,10 +2,7 @@ package com.example.timefor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,17 +10,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView textView = findViewById(R.id.textView2);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSignIn();
-            }
-        });
-    }
-
-    private void openSignIn() {
-        Intent intent = new Intent (this, signin_activity.class);
-        startActivity(intent);
     }
 }
